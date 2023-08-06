8503a4d22e1e933fc54e4674b14f65f0544097bd
Johny <37181988+JohnCale47@users.noreply.github.com>
2023-08-06 19:52:44 +0530

    v2
    adds URL Prefix "youtube.com/watch?v=" to video IDs

---
+++ extractorv2.py
@@ -0,0 +1,46 @@
+import os
+import json
+import googleapiclient.discovery
+
+def get_channel_videos(api_key, channel_id):
+    youtube = googleapiclient.discovery.build("youtube", "v3", developerKey=api_key)
+
+    # Get the playlist ID of the channel's uploads
+    response = youtube.channels().list(
+        part="contentDetails",
+        id=channel_id
+    ).execute()
+    
+    playlist_id = response["items"][0]["contentDetails"]["relatedPlaylists"]["uploads"]
+
+    # Get all video IDs from the channel's uploads playlist
+    videos = []
+    next_page_token = None
+
+    while True:
+        response = youtube.playlistItems().list(
+            part="snippet",
+            playlistId=playlist_id,
+            maxResults=50,
+            pageToken=next_page_token
+        ).execute()
+
+        videos.extend([item["snippet"]["resourceId"]["videoId"] for item in response["items"]])
+
+        next_page_token = response.get("nextPageToken")
+
+        if not next_page_token:
+            break
+
+    return videos
+
+if __name__ == "__main__":
+    API_KEY = "AIzaSyDpu-yw-E-y0ZJAGI-A4RPmC4SPxr_lxiY" # Replace it with YouTube Data API 
+    CHANNEL_ID = "UClYNH0Ho5mX2cf2pxg4qGGA"  # Replace with the ID of the YouTube channel you want to extract videos from
+
+    videos = get_channel_videos(API_KEY, CHANNEL_ID)
+    
+    video_links = [f"https://www.youtube.com/watch?v={video_id}" for video_id in videos]
+    
+    for link in video_links:
+        print(link)
